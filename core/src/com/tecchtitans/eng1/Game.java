30c1d8860c6961d3c41eb7a4e005bf695df02608
Bradley Mitchell <bm1293@its.york.ac.uk>
2024-03-13 13:23:10 +0000

    testing outputting tilemaps

--- core/src/com/tecchtitans/eng1/Game.java
+++ core/src/com/tecchtitans/eng1/Game.java
@@ -1,26 +1,47 @@
 package com.tecchtitans.eng1;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	private OrthographicCamera camera;
+	TiledMap map;
+	TiledMapRenderer mapRenderer;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 100, 100);
+		map = new TmxMapLoader().load("testmap2.tmx");
+		mapRenderer = new OrthogonalTiledMapRenderer(map);
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		//batch.begin();
+		//batch.draw(img, 0, 0);
+		//batch.end();
+
+		camera.update();
+		mapRenderer.setView(camera);
+		mapRenderer.render();
 	}
 	
 	@Override
