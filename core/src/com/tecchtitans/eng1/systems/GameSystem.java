bcf7504f17ad941d928aca2c181bb8e44c24c8c9
brad2307 <38055335+brad2307@users.noreply.github.com>
2024-03-17 21:46:13 +0000

    made it so the game ends after 7 days. outputs final study level and counter of each activity. also changed so that hour must be less than or equal to 16 before day ends

--- core/src/com/tecchtitans/eng1/systems/GameSystem.java
+++ core/src/com/tecchtitans/eng1/systems/GameSystem.java
@@ -12,9 +12,11 @@ public class GameSystem extends EntitySystem {
     private ImmutableArray<Entity> entities;
     private int hour;
     private int day;
+    private boolean gameComplete;
     public void addedToEngine(Engine engine) {
         hour = 0;
         day = 1;
+        gameComplete = false;
         entities = engine.getEntitiesFor(Family.all(PlayerComponent.class).get());
     }
 
@@ -24,48 +26,53 @@ public class GameSystem extends EntitySystem {
             ActivityComponent currentActivity = playerComponent.currentActivity;
 
             if(currentActivity != null){
-                /*switch (currentActivity.type) {
-                    case STUDY:
-                        break;
-                    case SLEEP:
-                        break;
-                    case REC:
-                        break;
-                    case EAT:
-                        break;
-                    default:
-                        break;
-                }*/
-                System.out.println("Activity type: " + currentActivity.type);
-                System.out.println("Current time: " + hour);
-                System.out.println("Current day: " + day);
-                System.out.println("Current energy: " + playerComponent.energy);
-                System.out.println("Current study: " + playerComponent.study);
-
-                //dont like how long this line is, split
-                if(hour + currentActivity.timeChange < 16 && playerComponent.energy + currentActivity.energyChange >= 0) {
-                    playerComponent.activityCount.put(
-                            currentActivity.type,
-                            playerComponent.activityCount.get(currentActivity.type) + 1);
-                    System.out.println("Activity performed");
-                    if(currentActivity.type == ActivityType.SLEEP) {
-                        day++;
-                        hour = 0;
-                        playerComponent.energy = 100;
-                    }
-                    else{
-                        hour += currentActivity.timeChange;
-                        playerComponent.energy += currentActivity.energyChange;
-                        playerComponent.study += currentActivity.studyChange;
-                    }
-
-                    System.out.println("New hour: " + hour);
-                    System.out.println("New energy: " + playerComponent.energy);
-                    System.out.println("New study: " + playerComponent.study);
+                if(gameComplete) {
+                    System.out.println("Game finished. Stats:");
+                    System.out.printf("SLEEP: %s, STUDY: %s, REC: %s, EAT: %s%n",
+                            playerComponent.activityCount.get(ActivityType.SLEEP),
+                            playerComponent.activityCount.get(ActivityType.STUDY),
+                            playerComponent.activityCount.get(ActivityType.REC),
+                            playerComponent.activityCount.get(ActivityType.EAT)
+                    );
+                    System.out.printf("Final study level: %s%n", playerComponent.study);
                 }
                 else {
-                    System.out.println("Activity not performed");
+                    System.out.println("Activity type: " + currentActivity.type);
+                    System.out.println("Current time: " + hour);
+                    System.out.println("Current day: " + day);
+                    System.out.println("Current energy: " + playerComponent.energy);
+                    System.out.println("Current study: " + playerComponent.study);
+
+                    //dont like how long this line is, split
+                    if(hour + currentActivity.timeChange <= 16 && playerComponent.energy + currentActivity.energyChange >= 0) {
+                        playerComponent.activityCount.put(
+                                currentActivity.type,
+                                playerComponent.activityCount.get(currentActivity.type) + 1);
+                        System.out.println("Activity performed");
+                        if(currentActivity.type == ActivityType.SLEEP) {
+                            day++;
+                            hour = 0;
+                            playerComponent.energy = 100;
+                            if(day >=  7) {
+                                gameComplete = true;
+                            }
+                        }
+                        else{
+                            hour += currentActivity.timeChange;
+                            playerComponent.energy += currentActivity.energyChange;
+                            playerComponent.study += currentActivity.studyChange;
+                        }
+
+                        System.out.println("New day: " + day);
+                        System.out.println("New hour: " + hour);
+                        System.out.println("New energy: " + playerComponent.energy);
+                        System.out.println("New study: " + playerComponent.study);
+                    }
+                    else {
+                        System.out.println("Activity not performed");
+                    }
                 }
+
 
                 playerComponent.currentActivity = null;
                 System.out.println("");
